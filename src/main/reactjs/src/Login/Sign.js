60150a49cb45fbdd4a67536643cb95558a3c29f1
Unic31 <uenic31@gmail.com>
2023-07-03 14:42:40 +0900

    test2

---
+++ src/main/reactjs/src/Login/Sign.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Sign(props) {
+    return (
+        <div>회원가입</div>
+    );
+}
+
+export default Sign;
---
+++ src/main/reactjs/src/Login/index.js
@@ -0,0 +1,2 @@
+export {default as Login} from './Login';
+export {default as Sign} from './Sign';
