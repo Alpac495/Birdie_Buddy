7e858b466d2309eb863eadaea7f3fd5bc7145688
tlstmddn683 <tlstmddn683@gmail.com>
2023-07-04 17:18:13 +0900

    ssw 0704 17:17 hugilist rowlist 수정

--- src/main/reactjs/src/hugi/HugiRowList.js
+++ src/main/reactjs/src/hugi/HugiRowList.js
@@ -1,24 +1,32 @@
 import React from 'react';
 import './List.css';
 import Avatar from '@mui/material/Avatar';
+import MessageIcon from '@mui/icons-material/Message';
+import {useNavigate} from "react-router-dom";
 function HugiRowList(props) {
     const {uname, hcontent, hphoto} = props;
-    const url = process.env.REACT_APP_BOARDURL;
-
+    // const url = process.env.REACT_APP_BOARDURL;
+    const navi=useNavigate();
+    const DetailButton=(e)=>{
+        navi("/hugi/detail");
+    }
     return (
         <div className="list">
             <div className="list_header">
                 <Avatar className="list_avatar" alt={uname}
                         src="/static/images/avatar/1.jpg"
                         />
-                <h3>{uname}</h3>s
-
-            <img className="list_image" src={`${url}${hphoto}`} alt="" />
-            <h4 className="list_text">
-                <strong>{uname}</strong>
+                <h5>{uname}</h5>
+            </div>
+            {/*<img className="list_image" src={`${url}${hphoto}`} alt="" />*/}
+            <img className="list_image" src={process.env.PUBLIC_URL +"/image/1.png"} alt="" />
+            <h5 className="list_text">
+                &nbsp;&nbsp;
                 {hcontent}
-            </h4>
-        </div>
+            </h5>
+                <MessageIcon style={{width:"25px",margin:"10px 10px",cursor:"pointer"}} onClick={DetailButton}/>
+
+
         </div>
     );
 }
