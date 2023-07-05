45f750c8c9920f0be6a2754f423f8ab187005db0
tlstmddn683 <tlstmddn683@gmail.com>
2023-07-05 16:15:46 +0900

    ssw 0705 commit!

--- src/main/reactjs/src/hugi/HugiRowList.js
+++ src/main/reactjs/src/hugi/HugiRowList.js
@@ -2,14 +2,16 @@ import React from 'react';
 import './List.css';
 import Avatar from '@mui/material/Avatar';
 import MessageIcon from '@mui/icons-material/Message';
+import ShareIcon from '@mui/icons-material/Share';
 import {useNavigate} from "react-router-dom";
 function HugiRowList(props) {
-    const {uname, hcontent, hphoto} = props;
+    const {uname, hcontent, hphoto,hwriteday} = props;
     // const url = process.env.REACT_APP_BOARDURL;
     const navi=useNavigate();
     const DetailButton=(e)=>{
         navi("/hugi/detail");
     }
+
     return (
         <div className="list">
             <div className="list_header">
@@ -18,6 +20,9 @@ function HugiRowList(props) {
                         />
                 <h5>{uname}</h5>
             </div>
+            <div style={{textAlign:"right"}}>
+            <span>{hwriteday}</span>
+            </div>
             {/*<img className="list_image" src={`${url}${hphoto}`} alt="" />*/}
             <img className="list_image" src={process.env.PUBLIC_URL +"/image/1.png"} alt="" />
             <h5 className="list_text">
@@ -25,7 +30,6 @@ function HugiRowList(props) {
                 {hcontent}
             </h5>
                 <MessageIcon style={{width:"25px",margin:"10px 10px",cursor:"pointer"}} onClick={DetailButton}/>
-
 
         </div>
     );
