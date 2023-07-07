77a1ed013d1b23f6db84b256594fadc8d5f6e760
jihyunnee <ldlwlgus0076@gmail.com>
2023-07-07 21:54:32 +0900

    0707 JoinList css 수정완료 충돌, 친구 목록 출력중

--- src/main/reactjs/src/friend/FriendList.js
+++ src/main/reactjs/src/friend/FriendList.js
@@ -1,22 +1,22 @@
+import "./FriendList.css";
 import React from 'react';
-import "./Friend.css";
-
-function FriendList(props) {
-
-    const {idx,item} = props;
-
+const FriendList = (props) => {
+    const {idx,item}=props;
     return (
-        <div>
-            <div className="rectangle-parent">
-                <div className="group-child" />
-                <div className="group-item" />
-                <b className="frlb">{item.uname}</b>
-                <div className="frldiv2">{item.ugender} {item.uage}</div>
-                <div className="frldiv3">평균타수 : 8.0</div>
+        <div className="friendlist">
+            <div className="friendlist-child" />
+            <div className="friendlist-item" />
+            <div className="flist">
+                <div className="flist-child" />
+                <div className="flist-item" />
+                <b className="b">{item.name}</b>
+                <div className="div">여 30</div>
+                <div className="div1">{idx+1}평균타수 : 8.0</div>
             </div>
-            <div className="friend-inner" /><br/>
+            <div className="flist-line" />
+
         </div>
     );
-}
+};
 
 export default FriendList;
