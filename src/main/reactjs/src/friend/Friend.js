c6f4ab040bf7f32a13a6aa2d3ae82f89f89061d2
jihyunnee <ldlwlgus0076@gmail.com>
2023-07-08 17:26:06 +0900

    0708 친구목록 css 수정중, JoinDetail 화면출력 test

--- src/main/reactjs/src/friend/Friend.js
+++ src/main/reactjs/src/friend/Friend.js
@@ -2,7 +2,6 @@ import React, {useCallback, useEffect, useState} from 'react';
 import {NavLink} from "react-router-dom";
 import Axios from "axios";
 import "./FriendList.css";
-import FriendList from "./FriendList";
 
 function Friend(props) {
     const unum=sessionStorage.unum;
@@ -22,34 +21,28 @@ function Friend(props) {
 
 
     return (
-        <div className="friend">
+        <div className="friendlist">
             <h4>총 친구수 : {data.length}</h4>
+
+            <div className="flist-line" />
             <div className="flframe">
-                <div className="div2">친구 목록</div>
+                <div className="FLdiv2">친구 목록</div>
             </div>
             <div className="flframe1">
-                <div className="div2">채팅</div>
+                <div className="FLdiv2">채팅</div>
             </div>
-            <div className="friend-child" />
-            <div className="frldiv">친구 목록</div>
-            <div className="frldiv1">채팅</div>
-            <div className="friend-item" />
 
             {
                 data.map &&
                 data.map((item,idx)=>
-                    <div className="friendlist">
-                        <div className="friendlist-child" />
-                        <div className="friendlist-item" />
                         <div className="flist">
-                            <div className="flist-child" />
+                            <div className="flist-child">
                             <div className="flist-item" />
-                            <b className="b">{item.uname}</b>
-                            <div className="div">여 30</div>
-                            <div className="div1">평균타수 : 8.0</div>
-                        </div>
-                        <div className="flist-line" />
+                            <b className="FLb">{item.uname}({item.unickname})</b>
+                            <div className="FLdiv">{item.ugender} /{item.uage}</div>
+                            <div className="FLdiv1">{item.ucontent}</div>
 
+                            </div>
                     </div>)
             }
 
