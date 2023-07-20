dc525f65a4e4f9e2720c0781e2207ea2ff6d7ecb
Jeon1301 <116734433+Jeon1301@users.noreply.github.com>
2023-07-20 18:26:32 +0900

    0720 jbj
    양도 디자인 수정중

--- src/main/reactjs/src/yangdo/Test.js
+++ src/main/reactjs/src/yangdo/Test.js
@@ -1,11 +1,86 @@
-import React from 'react';
-
-function Test(props) {
-    return (
-        <div>
-            
+import "./Test.css";
+const YangdoDetail1 = () => {
+  return (
+    <div className="yangdodetail1">
+      <div className="greenstroke-parent">
+        <div className="greenstroke">
+          <div className="frame-parent">
+            <div className="wrapper">
+              <div className="div">#포레스트힐</div>
+            </div>
+            <div className="frame-group">
+              <div className="label-wrapper">
+                <div className="label">2023.07.08 09:32 등록</div>
+              </div>
+              <div className="group-parent">
+                <img className="group-child" alt="" src="/group-74.svg" />
+                <div className="div1">수정</div>
+              </div>
+            </div>
+            <div className="group-container">
+              <div className="list-item-group-subtitle-parent">
+                <div className="list-item-group-subtitle">
+                  <div className="subtitle">
+                    <div className="label1">{`위약금 규정 `}</div>
+                  </div>
+                </div>
+                <div className="single-line-item">
+                  <div className="label2">골프장 규정에 따름</div>
+                </div>
+              </div>
+              <div className="single-line-item-parent">
+                <div className="single-line-item1">
+                  <img className="time-icon" alt="" src="/time.svg" />
+                  <div className="label2">07.21(금) 13:29</div>
+                </div>
+                <div className="single-line-item2">
+                  <img className="time-icon" alt="" src="/payment.svg" />
+                  <div className="label2">79000원</div>
+                </div>
+                <div className="single-line-item3">
+                  <img className="icon-note-1" alt="" src="/-icon-note-1.svg" />
+                  <div className="label2">상세 내용</div>
+                  <div className="secondary-action" />
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-    );
-}
+        <div className="frame-container">
+          <div className="parent">
+            <div className="div2">양도자 정보</div>
+            <div className="div3">신고하기</div>
+          </div>
+          <div className="flistprofile">
+            <div className="flistprofile1">
+              <img className="jduphoto-icon" alt="" src="/jduphoto@2x.png" />
+              <div className="div4">
+                <span className="txt">
+                  <p className="p">닉네임(이름)</p>
+                  <p className="p1">성별, 나이</p>
+                </span>
+              </div>
+              <div className="rectangle-parent">
+                <div className="group-item" />
+                <div className="div5">채팅하기</div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="graylbtn-parent">
+          <div className="graylbtn">
+            <div className="graylbtn-child" />
+            <div className="div6">닫기</div>
+          </div>
+          <div className="popupbtn">
+            <div className="frame">
+              <div className="div7">양도 신청</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Test;
+export default YangdoDetail1;
