10300c441af4ad5349ed976512b752769211825c
Park Myungsoo <qkautnkr@gmail.com>
2023-07-30 11:51:22 +0900

    pms 0730 commit 각종 버튼 및 css 수정

--- src/main/reactjs/src/mypage/MypageSetting.js
+++ src/main/reactjs/src/mypage/MypageSetting.js
@@ -37,6 +37,21 @@ const MypageSetting = () => {
     const onMyYangdo = () => {
         navi(`/mypage/myyangdo/${unum}`);
     }
+    const onMyProfile = () => {
+        navi(`/mypage/mypage/${unum}`);
+    }
+    const onMyBuddy = () => {
+        navi(`/friend/list`);
+    }
+    const onMyJoin = () => {
+        navi(`/joining/makelist`);
+    }
+    const onMyRank = () => {
+        navi(`/score/list`);
+    }
+    const onMyStory = () => {
+        navi(`/hugi/list/${unum}`);
+    }
 
     return (
         <div className="mypagesetting">
@@ -49,11 +64,11 @@ const MypageSetting = () => {
                     </div>
                 </div>
 
-                <div className="MPSsingle-line-item">
+                <div className="MPSsingle-line-item" onClick={onMyProfile}>
                     <img className="account-icon" alt="" src={mypro} />
                     <div className="MPSlabel1">마이 프로필</div>
                 </div>
-                <div className="MPSsingle-line-item">
+                <div className="MPSsingle-line-item" onClick={onMyBuddy}>
                     <img className="account-icon" alt="" src={BuddyIcon} />
                     <div className="MPSlabel1">마이 버디</div>
                 </div>
@@ -64,20 +79,19 @@ const MypageSetting = () => {
                         <div className="MPSlabel">Features</div>
                     </div>
                 </div>
-                <div className="MPSsingle-line-item2">
+                <div className="MPSsingle-line-item2" onClick={onMyJoin}>
                     <img className="account-icon" alt="" src={JoinIcon} />
                     <div className="MPSlabel1">마이 조인</div>
                 </div>
-                <div className="MPSsingle-line-item"
-                    onClick={onMyYangdo}>
+                <div className="MPSsingle-line-item" onClick={onMyYangdo}>
                     <img className="account-icon" alt="" src={YangdoIcon} />
                     <div className="MPSlabel1">마이 양도</div>
                 </div>
-                <div className="MPSsingle-line-item">
+                <div className="MPSsingle-line-item" onClick={onMyRank}>
                     <img className="account-icon" alt="" src={rang} />
                     <div className="MPSlabel1">마이 랭킹</div>
                 </div>
-                <div className="MPSsingle-line-item">
+                <div className="MPSsingle-line-item" onClick={onMyStory}>
                     <img className="account-icon" alt="" src={BuddyStory} />
                     <div className="MPSlabel1">마이 스토리</div>
                 </div>
