09bc6bef9e811251954772cc58c47496dbaba08c
Park Myungsoo <qkautnkr@gmail.com>
2023-08-02 09:36:33 +0900

    pms 0802 commit

--- src/main/reactjs/src/app_effect/Recommendslider.js
+++ src/main/reactjs/src/app_effect/Recommendslider.js
@@ -9,7 +9,7 @@ const SimpleSlider = () => {
     const image2 = process.env.REACT_APP_IMAGE87;
     const user = process.env.REACT_APP_MAIN;
     const navi = useNavigate();
-    const [unum, setUnum] = useState('');
+    const [unum, setUnum] = useState('0');
     useEffect(() => {
         Axios.get('/apimain/reco')
             .then(res => {
